0e8c72741a35f8595ad5871ce95c09e810fce782
troegeraaron <troeger.aaron@gmail.com>
2025-05-12 22:48:56 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Python Todo App
+
+I made this app as a simple practice. It isn't a throw-away practice app...I actually use it. 
+
+## How to use
+
+My current version of python is 3.12.3, so I know the app works there. I can't say backwards compatibilty
+works. 
+
+This app is a command based app. Very simple, you add todo items, complete them, and then delete them. 
+
+The todo list is saved as a CSV file. The app creates that file on first run. If you decide to put the
+app in the system path, the file will be created at your current loction. I will change that at some point
+but wanted to give a heads up for the v1 app.
+
+## Why?
+
+I didn't want to just create another web based todo app that required a database or client side storage.
+I wanted to practice some python, but wanted a simple, yet useful app. I'm also learning vim and neovim
+along with tmux, so I thought to create a terminal based app.
+
+## Changelog
+
+- 5/12/2025
+
+v1: Initial commit
+    - Added initial code
---
+++ todo_list.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+
+import csv, os
+
+file = "todo-list.csv"
+if not os.path.exists(file):
+    with open(file, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['id', 'task', 'completed'])
+        print(f'{file} created')
+
+class TodoItem():
+    def __init__(self,id, task, completed):
+        self.id = id
+        self.task = task
+        self.completed = completed
+    def to_dict(self):
+        return {'id': self.id, 'task': self.task, 'completed': self.completed}
+
+def add_task(task):
+    with open(file, "r", newline="") as reader:   
+        len_list = csv.DictReader(reader)
+        new_id = len(list(len_list))
+    todo_item = TodoItem(new_id, task, 'not complete')
+    with open(file, "a", newline="") as f:
+        fieldnames = ['id', 'task', 'completed']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writerow(todo_item.to_dict())
+    display_tasks()
+
+def display_tasks():
+    with open(file, 'r') as f:
+        reader = csv.DictReader(f)
+        print("")
+        for num, line in enumerate(reader):
+            if line['completed'] == 'complete':
+                print(f"{num} - [x] {line['task']}")
+            else:
+                print(f"{num} - [ ] {line['task']}")
+        print("")
+
+def help():
+    print("add <task name> - add a new task")
+    print("display - display all tasks")
+    print("complete <task number>")
+    print("exit - close program")
+
+def complete_task(task_num):
+    old_data = []
+    with open(file, 'r', newline="") as data:
+        reader = csv.DictReader(data)
+        old_data = [x for x in reader]
+    with open(file, 'w') as f:
+        fieldnames = ['id', 'task', 'completed']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for index, value in enumerate(old_data):
+            if int(task_num) == int(index):
+                print(f"{task_num} updated")
+                writer.writerow({'id': index,'task': value['task'], "completed": 'complete'})
+            else:
+                writer.writerow({'id': index,'task': value['task'], "completed": value['completed']})
+    display_tasks()
+
+def delete_task(task_num):
+    old_data = []
+    with open(file, 'r', newline='') as data:
+        reader = csv.DictReader(data)
+        old_data = [x for x in reader]
+    with open(file, 'w') as f:
+        fieldnnames = ['id', 'task', 'completed']
+        writer = csv.DictWriter(f, fieldnames=fieldnnames)
+        writer.writeheader()
+        for index, value in enumerate(old_data):
+            if int(task_num) == int(index):
+                print(f"{task_num} deleted")
+            else:
+                writer.writerow({'id': index, 'task': value['task'], 'completed': value['completed']})
+    display_tasks()
+
+
+def main():
+    print("-- type help for commands")
+    display_tasks()
+    exit_command = False
+    while exit_command == False:
+        command = str(input("todo >> "))
+        command = command.split(" ", 1)
+        match command[0]:
+            case "exit":
+                exit_command = True
+            case "add":
+                add_task(command[1])
+            case "display":
+                display_tasks()
+            case "complete":
+                complete_task(command[1])
+            case "delete":
+                delete_task(command[1])
+            case "help":
+                help()
+
+if __name__ == "__main__":
+    main()
